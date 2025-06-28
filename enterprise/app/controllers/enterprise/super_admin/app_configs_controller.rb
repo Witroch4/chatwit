94cf393875e87dfdaaad0252b3cae07326547df7
Witalo Rocha <witalo_rocha@hotmail.com>
2025-06-28 10:50:06 -0300

    páginas novas de 404, 422, 500 e atualizações nos models

--- enterprise/app/controllers/enterprise/super_admin/app_configs_controller.rb
+++ enterprise/app/controllers/enterprise/super_admin/app_configs_controller.rb
@@ -11,6 +11,14 @@ module Enterprise::SuperAdmin::AppConfigsController
       @allowed_configs = internal_config_options
     when 'captain'
       @allowed_configs = %w[CAPTAIN_OPEN_AI_API_KEY CAPTAIN_OPEN_AI_MODEL CAPTAIN_FIRECRAWL_API_KEY]
+    when 'audit_logs'
+      @allowed_configs = %w[ENABLE_AUDIT_LOGS AUDIT_LOG_RETENTION_DAYS AUDIT_LOG_EXPORT_FORMAT]
+    when 'disable_branding'
+      @allowed_configs = %w[HIDE_CHATWOOT_BRANDING CUSTOM_POWERED_BY_TEXT REMOVE_WIDGET_BRANDING REMOVE_EMAIL_BRANDING]
+    when 'line'
+      @allowed_configs = %w[LINE_CHANNEL_ID LINE_CHANNEL_SECRET LINE_CHANNEL_ACCESS_TOKEN LINE_WEBHOOK_URL]
+    when 'help_center'
+      @allowed_configs = %w[HELP_CENTER_DOMAIN HELP_CENTER_THEME HELP_CENTER_CUSTOM_CSS HELP_CENTER_ANALYTICS_ID]
     else
       super
     end
